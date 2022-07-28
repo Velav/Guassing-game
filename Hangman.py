588773c6b25ddc932ae1131653bab36981e9e11d
Velav <109713051+Velav@users.noreply.github.com>
2022-07-28 09:47:26 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,54 @@
+import random
+
+name = input("What is your name???")
+
+print("Good luck", name,"!!!")
+
+words = ['gaming', 'greenville', 'snowrunner', 'ets2', 'ats', 'msfs', 'Swfl', 'fs22', 'freeways', 'trailmakers',
+         'bloxbrug', 'ptfs', 'cdid', 'flightline', 'erlc', 'bedwars']
+
+word = random.choice(words)
+
+print("Guess the characters of the word!")
+
+guesses = ''
+
+turns = 12
+
+while turns > 0:
+    
+    failed = 0
+
+    for char in word:
+        
+        if char in guesses:
+            print(char)
+
+        else:
+            print("_")
+
+            failed += 1
+        
+    if failed == 0:
+        print("You Win, Congratulations!!!")
+
+        print("The word is: ", word)
+        break
+    guess = input("guess a charchter:")
+
+    guess += guess
+
+    if guess not in word:
+
+        turns -=1
+
+        print("Wrong")
+
+        print("You have", + turns, 'more guesses')
+
+        print("Wrong")
+
+        print("You have", + turns, 'more guesses')
+
+        if turns == 0:
+            print("You Loose, better luck next time!")
